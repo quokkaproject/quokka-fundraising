2d89d4eb678b6823934eeba4ef0d06bfa5245890
Bruno Rocha <rochacbruno@gmail.com>
2013-11-20 04:12:36 -0200

    admin fields organization and model clean

--- admin.py
+++ admin.py
@@ -2,91 +2,54 @@
 
 from quokka import admin
 from quokka.core.admin import _, _l
-from quokka.core.admin.models import ModelAdmin
+from quokka.core.admin.models import ModelAdmin, BaseContentAdmin
+from quokka.core.widgets import TextEditor, PrepopulatedText
 
 from .models import Campaign, Donation
 
 
-class CampaignAdmin(ModelAdmin):
-    """
-    All attributes added here for example
-    more info in admin source
-    github.com/mrjoes/flask-admin/blob/master/flask_admin/model/base.py
-    or Flask-admin documentation
-    """
+class CampaignAdmin(BaseContentAdmin):
 
-    roles_accepted = ('admin', 'editor')
-    can_create = True
-    can_edit = True
-    can_delete = True
+    column_searchable_list = ('title', 'description', 'summary')
 
-    # list_template = 'admin/model/list.html'
-    # edit_template = 'admin/custom/edit.html'
-    # create_template = 'admin/custom/create.html'
+    form_args = {
+        "description": {"widget": TextEditor()},
+        "slug": {"widget": PrepopulatedText(master='title')}
+    }
 
-    # column_list = ('title', 'slug', 'channel', 'published', 'view_on_site')
+    form_columns = ['title', 'slug', 'channel', 'related_channels', 'summary',
+                    'content_format', 'description', 'published',
+                    'start_date', 'end_date',
+                    'min_value', 'max_value',
+                    'goal', 'balance',
+                    'open_for_donations',
+                    'contents',
+                    'show_on_channel', 'available_at', 'available_until',
+                    'tags', 'values', 'template_type', 'donations']
 
-    #def view_on_site(self, request, obj, fieldname, *args, **kwargs):
-    #    return html.a(
-    #        href=obj.get_absolute_url('detail'),
-    #        target='_blank',
-    #    )(html.i(class_="icon icon-eye-open", style="margin-right: 5px;")(),
-    #      lazy_gettext('View on site'))
-
-    # column_exclude_list = []
-
-    # column_formatters = {'view_on_site': view_on_site}
-    # column_type_formatters = {}
-    # column_labels = {}
-    # column_descriptions = {}
-    # column_sortable_list = [] / ('name', ('user', 'user.username'))
-    # column_default_sort = 'pk'
-    # column_choices = {'column': ('value', 'display')}
-    # column_display_pk = True
-    # column_filters = ['published', 'title']
-    # column_searchable_list = ('title', 'body', 'summary')
-
-    # form_columns = ['title', 'slug', 'channel', 'related_channels',
-    #                 'summary',
-    #                'body', 'main_image', 'main_image_caption', 'published',
-    #                'show_on_channel', 'available_at', 'tags', 'comments',
-    #                'values']
-    # form_excluded_columns = []
-    # form = None
-    # form_overrides = None
-
-    # form_widget_args = {
-    #    'body': {
-    #        'rows': 20,
-    #        'cols': 20,
-    #        'class': 'text_editor',
-    #        'style': "margin: 0px; width: 725px; height: 360px;"
-    #    },
-    #    'summary': {
-    #        'style': 'width: 400px; height: 100px;'
-    #    },
-    #    'title': {'style': 'width: 400px'},
-    #    'slug': {'style': 'width: 400px'},
-    #}
-
-    # form_args = {
-    #     'body': {
-    #         'widget': BigTextArea()
-    #     }
-    # }
-
-    # form_extra_fields = {}
-
-    # action_disallowed_list
-
-    # page_size = 20
-    #form_ajax_refs = {
-    #    'main_image': {"fields": ('title',)}
-    #}
+    form_subdocuments = {
+        'contents': {
+            'form_subdocuments': {
+                None: {
+                    'form_columns': ('content', 'caption', 'purpose', 'order'),
+                    'form_ajax_refs': {
+                        'content': {
+                            'fields': ['title', 'long_slug', 'summary']
+                        }
+                    }
+                }
+            }
+        },
+    }
 
 
 class DonationAdmin(ModelAdmin):
     roles_accepted = ('admin', 'editor')
+    column_list = ['donor', 'created_at', 'values', 'total', 'tax',
+                   'status', 'published']
+    form_columns = ['donor', 'created_at', 'values', 'total', 'tax',
+                    'status', 'published',
+                    'confirmed_date']
 
 
 admin.register(Campaign, CampaignAdmin,
--- models.py
+++ models.py
@@ -1,36 +1,110 @@
 # coding: utf-8
 
+import datetime
 from quokka.core.db import db
+from quokka.utils import get_current_user
 from quokka.core.models import Publishable
-from quokka.modules.cart.models import BaseProduct
+from quokka.modules.cart.models import BaseProduct, BaseProductReference, Cart
+
+
+class Donations(db.EmbeddedDocument):
+    donation = db.ReferenceField('Donation')
+    status = db.StringField(default="pending", max_length=255)
+    value = db.FloatField(default=0)
+    donor = db.StringField(max_length=255)
+    show_donor = db.BooleanField(default=True)
 
 
 class Campaign(BaseProduct):
-    body = db.StringField(required=True)
-    start_date = db.DateTimeField()
+    description = db.StringField(required=True)
+    start_date = db.DateTimeField(default=datetime.datetime.now)
     end_date = db.DateTimeField()
-    min_value = db.FloatField()
+    min_value = db.FloatField(default=0)
     max_value = db.FloatField()
     goal = db.FloatField()
-    balance = db.FloatField()
+    balance = db.FloatField(default=0)
     open_for_donations = db.BooleanField(default=True)
+    donations = db.ListField(db.EmbeddedDocumentField(Donations))
+
+    def __unicode__(self):
+        return self.title
+
+    def update_donation(self, donation, value):
+        don = self.donations.get(donation=donation)
+        if don:
+            don.status = donation.status
+            don.value = value
+            don.donor = donation.donor.name
+            don.show_donor = donation.published
+        else:
+            don = Donations(
+                donation=donation,
+                status=donation.status,
+                value=value,
+                donor=donation.donor.name,
+                show_donor=donation.published
+            )
+            self.donations.append(don)
+        self.save()
+
+    def get_donor_list(self):
+        return [
+            donation.donor
+            for donation in
+            self.donations.filter(show_donor=True, status="confirmed")
+        ]
+
+    def save(self, *args, **kwargs):
+        self.balance = sum([item.value for item in self.donations])
+        super(Campaign, self).save(*args, **kwargs)
 
 
 class Values(db.EmbeddedDocument):
     campaign = db.ReferenceField(Campaign)
-    value = db.FloatField()
+    value = db.FloatField(default=0)
+
+    def __unicode__(self):
+        return u"{s.campaign} - {s.value}".format(s=self)
 
 
-class Donation(Publishable, db.DynamicDocument):
-    status = db.StringField()
+class Donation(BaseProductReference, Publishable, db.DynamicDocument):
+    status = db.StringField(default="pending", max_length=255)
     values = db.ListField(db.EmbeddedDocumentField(Values))
-    total = db.FloatField()
-    tax = db.FloatField()
+    total = db.FloatField(default=0)
+    tax = db.FloatField(default=0)
+    donor = db.ReferenceField('User', default=get_current_user, required=True)
 
-    @property
-    def donor(self):
-        return self.created_by
+    cart = db.ReferenceField(Cart, reverse_delete_rule=db.NULLIFY)
+    confirmed_date = db.DateTimeField()
 
-    @donor.setter
-    def donor(self, value):
-        self.created_by = value
+    def __unicode__(self):
+        return u"{s.donor} - {s.total}".format(s=self)
+
+    def set_status(self, status, *args, **kwargs):
+        self.status = status
+        if status == "confirmed":
+            now = datetime.datetime.now()
+            self.confirmed_date = kwargs.get('date', now)
+        self.save()
+
+    def clean(self):
+        unique_values = {
+            unique: 0
+            for unique in set([item.campaign for item in self.values])
+        }
+        for item in self.values:
+            unique_values[item.campaign] += item.value
+
+        self.values = [
+            Values(campaign=campaign, value=value)
+            for campaign, value in unique_values.items()
+        ]
+
+        if self.values:
+            self.total = sum([item.value for item in self.values])
+
+    def save(self, *args, **kwargs):
+        super(Donation, self).save(*args, **kwargs)
+
+        for item in self.values:
+            item.campaign.update_donation(self, item.value)
